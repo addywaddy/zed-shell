5f30fee2df7205de723513ba3362d48ef7b68636
Adam Groves <adam.groves@gmail.com>
2016-12-21 12:08:29 +0100

    Don't need these other paths

--- zshenv.zsh
+++ zshenv.zsh
@@ -4,14 +4,8 @@ export LC_CTYPE=$LANG
 # Path
 export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
-export PATH=$PATH:/Users/addywaddy/Scripts:/Users/addywaddy/bin
-export PATH=$PATH:/Users/addywaddy/Repos/github/ansible/hacking
-export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH=$PATH:$HOME/Scripts
 export EDITOR="/usr/bin/vim"
 export VISUAL=$EDITOR
 
 export WORDCHARS="${WORDCHARS:s#/#}"
-# Java
-# export  JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
-# export  CLASSPATH=$CLASSPATH:/usr/local/Cellar/clojure-contrib/1.2.0/clojure-contrib.jar
-
