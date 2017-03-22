dfb6f9f6a0b0cd8a57caf396720486ecab973597
Adam Groves <adam.groves@gmail.com>
2017-03-22 11:19:33 +0100

    Using ag now instead of ack

--- functions.zsh
+++ functions.zsh
@@ -8,14 +8,14 @@ function gom () {
   cd `bundle show $1`
 }
 
-function sedack() {
+function sedag() {
 
   if [[ $1 == "-h" ]];
   then
     echo "Usage:"
-    echo "sedack {flags} {ack_pattern} {sed_expression} {folders}\n"
+    echo "sedag {flags} {ag_pattern} {sed_expression} {folders}\n"
     echo "Example:"
-    echo 'sedack -i "Hello" "Hello/Goodbye" lib app\n'
+    echo 'sedag -i "Hello" "Hello/Goodbye" lib app\n'
     echo "Flags:"
     echo "-i for realz\n"
     return 1
@@ -27,12 +27,12 @@ function sedack() {
   if [[ $1 == "-i" ]];
   then
     echo "Replacing '$2' with '$3' in the following files:\n"
-    ack -a -l $2 ${@: 4}
-    ack -a -l  --print0 $2 ${@: 4} |xargs -0 -n 1 sed -i '' -e "s|$2|$3|"
+    ag -a -l $2 ${@: 4}
+    ag -a -l  --print0 $2 ${@: 4} |xargs -0 -n 1 sed -i '' -e "s|$2|$3|"
     echo "\nDone!"
   else
     echo "Would run the substitution '$2' in the following files:\n"
-     ack -a -l $1 ${@: 3}
+     ag -a -l $1 ${@: 3}
   fi
   LANG=$ORIG_LANG
 }
